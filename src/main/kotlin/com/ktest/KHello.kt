f46deb3d09b579664aadbf6db74780e712177c3c
chocowind797 <ziyun5257055@gmail.com>
2021-03-06 09:41:29 +0800

    Kotlin

--- src/main/kotlin/com/ktest/KHello.kt
+++ src/main/kotlin/com/ktest/KHello.kt
@@ -12,7 +12,8 @@ typealias MyHandler = (Int, String, Any) -> Unit
 
 fun main(args: Array<String>) {
     println("Hello Kotlin")
-
+    Thread.sleep(1000)  // kotlin 不強迫使用 try-catch
+    println("Hello Kotlin")
     val j = JHello()
     j.callMe()
 
