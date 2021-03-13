c34c657af23a906fa6016dbdc8873c36d501176b
chocowind797 <ziyun5257055@gmail.com>
2021-03-13 15:41:59 +0800

    Kotlin 解決實作衝突

--- src/main/kotlin/com/類別與物件/介面與抽象/KInterface.kt
+++ src/main/kotlin/com/類別與物件/介面與抽象/KInterface.kt
@@ -3,13 +3,35 @@ package com.類別與物件.介面與抽象
 interface Clickable {
     fun click()     // 一般抽象方法
     fun showOff() { // 類似 Java 8 介面的 default 方法
-        println("I am clickable")
+        println("I am clickable.")
+    }
+}
+
+interface Focusable {
+    fun setFocus(b: Boolean)
+    fun showOff() { // 類似 Java 8 介面的 default 方法
+        println("I am focusable.")
     }
 }
 
 class Button: Clickable {
     override fun click() {
         println("Button clicked!")
+    }
+}
+
+class TextField: Clickable, Focusable {
+    override fun click() {
+        println("TextField clicked!")
+    }
+
+    override fun setFocus(b: Boolean) {
+        println("I ${if(b) "got" else "lost"} focus.")
+    }
+
+    override fun showOff() {
+        super<Clickable>.showOff()
+        super<Focusable>.showOff()
     }
 }
 
