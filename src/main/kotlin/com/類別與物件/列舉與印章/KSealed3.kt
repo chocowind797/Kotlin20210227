ad1bd34414b891e9519b24a9a1f866ac892c87cc
chocowind797 <ziyun5257055@gmail.com>
2021-03-20 11:27:07 +0800

    Kotlin 印章(Sealed) 應用

--- src/main/kotlin/com/類別與物件/列舉與印章/KSealed3.kt
+++ src/main/kotlin/com/類別與物件/列舉與印章/KSealed3.kt
@@ -1,16 +1,16 @@
 package com.類別與物件.列舉與印章
 
-sealed class Operation {
-    class Add(val x: Int, val y: Int): Operation()
-    class Sub(val x: Int, val y: Int): Operation()
-}
+ sealed class Operation {
+     class Add(val x: Int, val y: Int) : Operation()
+     class Sub(val x: Int, val y: Int) : Operation()
+ }
 
 fun exec(op: Operation): Int = when(op){
     is Operation.Add -> op.x + op.y
     is Operation.Sub -> op.x - op.y
 }
 
-//// 私自建立 Mul
+// 私自建立 Mul
 //class Mul(val x: Int, val y: Int): Operation()
 
 fun main() {
