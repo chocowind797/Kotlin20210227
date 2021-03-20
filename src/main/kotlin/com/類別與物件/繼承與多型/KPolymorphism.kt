02fa30861b65cfb207f5fae1a946048724d055a9
chocowind797 <ziyun5257055@gmail.com>
2021-03-20 10:12:04 +0800

    Kotlin 多型

--- src/main/kotlin/com/類別與物件/繼承與多型/KPolymorphism.kt
+++ src/main/kotlin/com/類別與物件/繼承與多型/KPolymorphism.kt
@@ -1,4 +1,24 @@
 package com.類別與物件.繼承與多型
 
-class KPolymorphism {
+abstract class Human(val name: String) {
+    abstract fun eat()
+}
+
+class Man(name: String): Human(name) {
+    override fun eat() {
+        println("$name 大口吃飯")
+    }
+}
+
+class Woman(name: String): Human(name) {
+    override fun eat() {
+        println("$name 小口吃飯")
+    }
+}
+
+fun main() {
+    val man = Man("男人")
+    val woman = Woman("女人")
+    val list = listOf(man, woman)
+    list.forEach{it.eat()}
 }
